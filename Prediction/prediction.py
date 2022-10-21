afb12e7aad3e4af3dcab255b9b277dee2a9e7d09
Kanan <76784825+Theynalzada@users.noreply.github.com>
2022-10-21 21:00:30 +0400

    Match Week 13 Predictions Update

--- Prediction/prediction.py
+++ Prediction/prediction.py
@@ -190,13 +190,13 @@ current_season_df = pd.read_csv(filepath_or_buffer = '/Users/kzeynalzade/Documen
 current_season_df.match_date = pd.to_datetime(arg = current_season_df.match_date, yearfirst = True)
 
 # Creating a list of match dates
-dates = pd.to_datetime(arg = ('2022-10-18 ' * 2).strip().split() + ('2022-10-19 ' * 5).strip().split() + ('2022-10-20 ' * 2).strip().split(), yearfirst = True)
+dates = pd.to_datetime(arg = ('2022-10-22 ' * 4).strip().split() + ('2022-10-23 ' * 5).strip().split() + ['2022-10-24'], yearfirst = True)
 
 # Creating a list of home teams
-home_teams = ['Brighton', 'Crystal Palace', 'Bournemouth', 'Brentford', 'Liverpool', 'Newcastle', 'Man Utd', 'Fulham', 'Leicester']
+home_teams = ['Nott\'m Forest', 'Everton', 'Man City', 'Chelsea', 'Aston Villa', 'Leeds', 'Southampton', 'Wolves', 'Spurs', 'West Ham']
 
 # Creating a list of away teams
-away_teams = ['Nott\'m Forest', 'Wolves', 'Southampton', 'Chelsea', 'West Ham', 'Everton', 'Spurs', 'Aston Villa', 'Leeds']
+away_teams = ['Liverpool', 'Crystal Palace', 'Brighton', 'Man Utd', 'Brentford', 'Fulham', 'Arsenal', 'Leicester', 'Newcastle', 'Bournemouth']
 
 # Defining a function to generate input data
 def generate_input_data(match_week = None, 
@@ -354,7 +354,7 @@ def make_predictions(data_frame = None, use_shadow_models = False):
         logging.info(msg = 'Shadow engine made predictions successfully')
 
 # Calling the function to generate input data
-input_data = generate_input_data(match_week = 12,
+input_data = generate_input_data(match_week = 13,
                                  match_dates = dates,
                                  home_teams = home_teams,
                                  away_teams = away_teams,
